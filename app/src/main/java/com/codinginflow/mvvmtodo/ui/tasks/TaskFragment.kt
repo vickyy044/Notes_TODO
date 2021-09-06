2ac675bc02034dcc96ca231cfde5d6a530723c1a
vickyy044 <43821289+vickyy044@users.noreply.github.com>
2021-09-06 18:50:20 +0530

    added menu in task fragment implemented Search Query in Room

--- app/src/main/java/com/codinginflow/mvvmtodo/ui/tasks/TaskFragment.kt
+++ app/src/main/java/com/codinginflow/mvvmtodo/ui/tasks/TaskFragment.kt
@@ -1,24 +1,29 @@
 package com.codinginflow.mvvmtodo.ui.tasks
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.codinginflow.mvvmtodo.R
 import com.codinginflow.mvvmtodo.databinding.FragmentTasksBinding
+import com.codinginflow.mvvmtodo.util.onQueryTextChanged
 import dagger.hilt.android.AndroidEntryPoint
 
 
 @AndroidEntryPoint
-class TaskFragment : Fragment(R.layout.fragment_tasks){
+class TaskFragment : Fragment(R.layout.fragment_tasks) {
 
-    private val viewModel : TaskViewModel by viewModels()
+    private val viewModel: TaskViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val binding  = FragmentTasksBinding.bind(view)
+        val binding = FragmentTasksBinding.bind(view)
         val taskAdapter = TasksAdapter()
         binding.apply {
             recyclerViewTask.apply {
@@ -28,10 +33,41 @@ class TaskFragment : Fragment(R.layout.fragment_tasks){
             }
         }
 
-        viewModel.tasks.observe(viewLifecycleOwner){
+        viewModel.tasks.observe(viewLifecycleOwner) {
             taskAdapter.submitList(it)
         }
+        setHasOptionsMenu(true)
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_fragment_task, menu)
+        val searchItem = menu.findItem(R.id.action_search)
+        val searchView = searchItem.actionView as SearchView
+
+        searchView.onQueryTextChanged {
+            //update search query
+            viewModel.searchQuery.value = it
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_sort_by_name -> {
+                true
+            }
+            R.id.action_hide_completed_tasks ->{
+                item.isChecked = !item.isChecked
+                true
+            }
+            R.id.action_sort_by_date_created -> {
+
+                true
+            }
+            R.id.action_delete_completed_tasks ->{
+                true
+            }else -> super.onOptionsItemSelected(item)
+
+        }
     }
 
 }
